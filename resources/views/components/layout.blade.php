b723597fd832746ef6872543da535165fc1c1ab9
John <johnhringiv@gmail.com>
2025-11-01 16:41:16 -0400

    Add link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -157,7 +157,7 @@
 
 <footer class="footer footer-center p-5 bg-base-300 text-base-content text-xs">
     <div>
-        <p>© {{ date('Y') }} Lightning Class Association - G.O.T. Flashes Challenge Tracker</p>
+        <p>© {{ date('Y') }} <a href="https://www.lightningclass.org/" target="_blank" rel="noopener noreferrer" class="link link-hover">Lightning Class Association</a> - G.O.T. Flashes Challenge Tracker</p>
         <p class="text-xs opacity-70">Track your sailing days • Earn awards • Build the Lightning community</p>
         <p class="text-xs opacity-60 mt-2">
             Created by <a href="https://johnhringiv.com/" target="_blank" rel="noopener noreferrer" class="link link-hover">John Ring</a>
